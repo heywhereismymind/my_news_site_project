32cf9b3c6a6ca6a248276ced53434a9eb5651f2c
zabor <zaborka18@gmail.com>
2024-03-14 20:55:46 +0300

    Registered app | Add related model User in Article

--- mywebsite/news/models.py
+++ mywebsite/news/models.py
@@ -1,24 +1,19 @@
 from django.db import models
 from django.utils import timezone
-
-
-class Reporter(models.Model):
-    full_name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.full_name
+from django.contrib.auth.models import User
 
 
 class Article(models.Model):
     publish = models.DateField(default=timezone.now)
     headline = models.CharField(max_length=200)
     content = models.TextField()
-    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news_articles')
 
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish'])
         ]
+
     def __str__(self):
         return self.headline
