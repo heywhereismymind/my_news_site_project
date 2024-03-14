e9f0d62f35b7fe33ce83883c8489ee878e36ad43
zabor <zaborka18@gmail.com>
2024-03-14 20:48:40 +0300

    Initialize project | Add models Reporter and Article

---
+++ mywebsite/news/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Article(models.Model):
+    publish = models.DateField(default=timezone.now)
+    headline = models.CharField(max_length=200)
+    content = models.TextField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
