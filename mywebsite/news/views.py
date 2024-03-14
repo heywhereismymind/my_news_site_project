b7782c8b590b630358c01707e81714c7b30429b0
zabor <zaborka18@gmail.com>
2024-03-14 21:13:12 +0300

    add views

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Article
+
+
+def article_detail(request, id):
+    article = get_object_or_404(Article, id=id)
+    return render(request, 'news/article/detail.html', {'article': article})
+
+
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'news/article/list.html', {'articles': articles})
