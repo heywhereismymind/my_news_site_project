7ec139ec1d06d1a1d48e11235ea1d339686ae332
zabor <zaborka18@gmail.com>
2024-03-26 19:21:46 +0300

    add new filters in views.py

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -5,10 +5,10 @@ from .models import Article
 
 
 def article_detail(request, id):
-    article = get_object_or_404(Article, id=id)
+    article = get_object_or_404(Article, id=id, status=Article.Status.PUBLISHED)
     return render(request, 'news/article/detail.html', {'article': article})
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.published.all()
     return render(request, 'news/article/list.html', {'articles': articles})
