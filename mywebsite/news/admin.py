2649a2e971b80628f1f101ec6bfc254181e64d8f
zabor <zaborka18@gmail.com>
2024-03-26 19:07:04 +0300

    change filters in admin.py

--- mywebsite/news/admin.py
+++ mywebsite/news/admin.py
@@ -5,9 +5,10 @@ from .models import Article
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ["headline", "reporter", "publish"]
-    list_filter = ["publish", "reporter"]
+    list_display = ["headline", "reporter", "publish", "status"]
+    list_filter = ["publish", "reporter", "status", "created"]
     search_fields = ["headline", "content"]
     raw_id_fields = ["reporter"]
     date_hierarchy = "publish"
-    ordering = ["publish"]
+    ordering = ["publish", "status"]
+    prepopulated_fields = {"slug": ("headline",)}
