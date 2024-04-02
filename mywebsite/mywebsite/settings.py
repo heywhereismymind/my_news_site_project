ffa330e731c44019d0595df61e2a9fa43e7ffbce
zabor <zaborka18@gmail.com>
2024-04-02 19:46:24 +0300

    Add 404.html

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2)%l#re@q@f+e8_in*dufoqq2z)sayx_cn%&*0))s#p4ge&0n-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
