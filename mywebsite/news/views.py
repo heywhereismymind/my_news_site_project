c7e0d187ae9d9fc6633df7b2e82bce9dbdcd20ad
zabor <zaborka18@gmail.com>
2024-04-02 21:08:28 +0300

    Add some logic with send_mail in article_share

--- mywebsite/news/views.py
+++ mywebsite/news/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
+from mywebsite.settings import EMAIL_HOST_USER
 
 from .models import Article
 
 
 def article_share(request, article_id):
     article = get_object_or_404(Article, id=article_id, status=Article.Status.PUBLISHED)
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST)
+
         if form.is_valid():
             cd = form.cleaned_data
+            article_url = request.build_absolute_uri(article.get_absolute_url())
+
+            subject = f"{cd['name']} recommends you read {article.headline}"
+
+            message = (
+                f"Read {article.headline} at {article_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+
+            send_mail(subject, message, EMAIL_HOST_USER, cd["to"])
+            sent = True
     else:
         form = EmailPostForm()
     return render(
