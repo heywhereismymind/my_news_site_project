bacb09202c6c3d28398968b875e1c192354cf755
Ivaniu_YI <Ivaniu_YI@npc.by>
2024-04-11 17:33:27 +0300

    Add latest articles in base.html

--- mywebsite/news/templatetags/news_tags.py
+++ mywebsite/news/templatetags/news_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from ..models import Article
 
 
@@ -10,7 +12,14 @@ def total_articles():
     return Article.published.count()
 
 
-@register.inclusion_tag("news/article/latest_news.html")
+@register.inclusion_tag("news/article/latest_articles.html")
 def show_latest_articles(count=3):
     latest_articles = Article.published.order_by("-publish")[:count]
     return {"latest_articles": latest_articles}
+
+
+@register.simple_tag
+def get_most_commented_articles(count=3):
+    return Article.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
