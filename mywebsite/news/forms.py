29cd5f79000eea57e256ed4e577219c6235d6040
Ivaniu_YI <Ivaniu_YI@npc.by>
2024-04-11 09:04:54 +0300

    add comments in form.py

--- mywebsite/news/forms.py
+++ mywebsite/news/forms.py
@@ -1,7 +1,14 @@
 from django import forms
+from mywebsite.news.models import Comment
 
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=255)
     to = forms.EmailField()
     comment = forms.CharField(widget=forms.Textarea, required=False)
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["name", "email", "body"]
