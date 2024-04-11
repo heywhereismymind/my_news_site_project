df50f328b1a20d5d313b815e37615e7688f5e332
Ivaniu_YI <Ivaniu_YI@npc.by>
2024-04-11 16:58:46 +0300

    register inclusion_tag

--- mywebsite/news/templatetags/news_tags.py
+++ mywebsite/news/templatetags/news_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 @register.simple_tag
 def total_articles():
     return Article.published.count()
+
+
+@register.inclusion_tag("news/article/latest_news.html")
+def show_latest_articles(count=3):
+    latest_articles = Article.published.order_by("-publish")[:count]
+    return {"latest_articles": latest_articles}
