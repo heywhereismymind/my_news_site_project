714889385f02ae592643ba21da303bb11e5cf70e
Ivaniu_YI <Ivaniu_YI@npc.by>
2024-04-11 08:59:06 +0300

    Reformat html\css | Add new model - Comment

--- mywebsite/news/forms.py
+++ mywebsite/news/forms.py
@@ -3,6 +3,5 @@ from django import forms
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=255)
-    # email = forms.EmailField()
     to = forms.EmailField()
     comment = forms.CharField(widget=forms.Textarea, required=False)
